4c43215ef44a54290be333b77fe192ec78b61823
Luca <luca.a.mugnaini@rakuten.com>
2017-01-18 10:38:02 +0100

    no message

--- source/scripts/download.girelloni.com.js
+++ source/scripts/download.girelloni.com.js
@@ -1,5 +1,11 @@
 "use strict";
 let downloadGoogleSpreadsheet = require("./downloadGoogleSpreadsheet");
 downloadGoogleSpreadsheet("1fW-LlMLBUNP6d7UaAP8C1PuBhKBOcJpE62dBYwr1vKA", (data) => { // blog
-	console.log(JSON.stringify(data, null, "\t"));
+	const unordered = data.sheets;
+	const ordered = {};
+	Object.keys(unordered).sort().forEach(function (key) {
+		ordered[key] = unordered[key];
+	});
+	data.sheets = ordered;
+	console.log(JSON.stringify(data, null, '\t'));
 });
